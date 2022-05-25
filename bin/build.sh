4dbf17eaa32e0935e420e99ff6c0ccfabed78f38
vipaol <vipaol@ideapad330>
2022-05-25 22:53:59 +0400

    build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -11,12 +11,16 @@
 # This batch file builds and preverifies the code for the demos.
 # it then packages them in a JAR file appropriately.
 #
+
+LIBS=../lib/PhysicsEngine_v135a.jar${PATHSEP}../../WTK2.5.2/lib/jsr75.jar
 APP=MobileApplication3
+MANIFEST=../build/manifest.mf
+
 LIB_DIR=../../../lib
-CLDCAPI=${LIB_DIR}/cldcapi10.jar
+CLDCAPI=${LIB_DIR}/cldcapi11.jar
 MIDPAPI=${LIB_DIR}/midpapi20.jar
-PREVERIFY=../../../bin/preverify
-JAVA_HOME=../../jdk1.6.0_45
+PREVERIFY=../../WTK2.5.2/bin/preverify
+JAVA_HOME=../../../../jdk1.6.0_45
 
 PATHSEP=":"
 
@@ -43,8 +47,8 @@ ${JAVAC} \
     -bootclasspath ${CLDCAPI}${PATHSEP}${MIDPAPI} \
     -source 1.3 \
     -target 1.3 \
-    -d ../tmpclasses \
-    -classpath ../tmpclasses \
+    -d ../lib \
+    -classpath ${LIBS}\
     `find ../src -name '*'.java`
 
 echo "Preverifying class files..."
@@ -55,7 +59,7 @@ ${PREVERIFY} \
     ../tmpclasses
 
 echo "Jaring preverified class files..."
-${JAR} cmf MANIFEST.MF ${APP}.jar -C ../classes .
+${JAR} cmf ${MANIFEST} ${APP}.jar -C ../classes .
 
 if [ -d ../res ] ; then
   ${JAR} uf ${APP}.jar -C ../res .
