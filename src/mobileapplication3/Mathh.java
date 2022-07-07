e451fc94b9a034142b9cde55708de4186645fdde
vipaol <vipaol@ideapad330>
2022-07-07 22:19:03 +0400

    try to add arctg

--- src/mobileapplication3/Mathh.java
+++ src/mobileapplication3/Mathh.java
@@ -9,8 +9,9 @@ package mobileapplication3;
  *
  * @author vipaol
  */
-public class Mathh { // надо будет перевести на стандартную
+public class Mathh {
     private static int sin_t[] = {0, 174, 342, 500, 643, 766, 866, 940, 985, 1000};
+    //private static int tg_t[] = {0, 105, 212, 324, 445, 577, 726, 900, 1110, 1376, 1732, 2246, 3077, 4704, 9514};
     private static int sinus(int t) {
         int k;
         k = (int) (t / 10);
@@ -54,5 +55,23 @@ public class Mathh { // надо будет перевести на станда
         } else {
             return sinus(t - 270);
         }
+    }
+    
+    /*public static int arctg(int x, int y) {
+        if (x == 0) {
+            if (y > 0) {
+                return 90;
+            } else {
+                return 270;
+            }
+        }
+        int tg = 1000 * y / x;
+        //*binary search in tg_t*
+    }*/
+    
+    static int distance(int x1, int y1, int x2, int y2) {
+        int dx = x1 - x2;
+        int dy = y1 - y2;
+        return (int) Math.sqrt(dx * dx + dy * dy);
     }
 }
