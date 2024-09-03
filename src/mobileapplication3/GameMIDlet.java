d6b39192369ff1f1d2c2184043befaf440ee745f
vipoll <vipollmail@gmail.com>
2024-09-03 23:48:31 +0400

    sync with the latest changes made for android

--- src/mobileapplication3/GameMIDlet.java
+++ src/mobileapplication3/GameMIDlet.java
@@ -4,13 +4,6 @@
  */
 package mobileapplication3;
 
-import java.io.IOException;
-
-import javax.microedition.lcdui.Alert;
-import javax.microedition.lcdui.AlertType;
-import javax.microedition.lcdui.Display;
-import javax.microedition.lcdui.Displayable;
-import javax.microedition.lcdui.Image;
 import javax.microedition.midlet.MIDlet;
 
 import mobileapplication3.game.MenuCanvas;
@@ -47,7 +40,7 @@ public class GameMIDlet extends MIDlet {
 
         Logger.log("Main:constr");
         MenuCanvas menuCanvas = new MenuCanvas();
-        set(new RootContainer(menuCanvas, null));
+        Platform.setCurrent(new RootContainer(menuCanvas, null));
     }
 
     public void pauseApp() {
@@ -59,50 +52,6 @@ public class GameMIDlet extends MIDlet {
     
     public static void exit() {
         thiss.destroyApp(true);
-    }
-    
-    public static void set(Displayable d) {
-        Display.getDisplay(thiss).setCurrent(d);
-        System.gc();
-    }
-    
-    public static void set(Alert a) {
-        Display.getDisplay(thiss).setCurrent(a, Display.getDisplay(thiss).getCurrent());
-        System.gc();
-    }
-    
-    public static void showAlert(Throwable ex) {
-        ex.printStackTrace();
-        showAlert(ex.toString());
-    }
-    public static void showAlert(String text) {
-        showAlert(text, -1);
-    }
-    public static void showAlert(String text, int duration) {
-        Alert alert = null;
-        try {
-            //AlertType.ERROR.playSound(thiss.display);
-            Image alertImage = null;
-            try {
-                alertImage = Image.createImage("/driver.png");
-            } catch (IOException ex1) {
-                try {
-                    alertImage = Image.createImage("resourse://driver.png");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Logger.log("Can't load alert image");
-                }
-            }
-            alert = new Alert("Oh no!", text, alertImage, AlertType.ERROR);
-            if (duration > 0) {
-                alert.setTimeout(duration);
-            }
-            Display.getDisplay(thiss).setCurrent(alert); // TODO: implement nextDisplayable
-        } catch(IllegalArgumentException ex) {
-            ex.printStackTrace();
-            Logger.log("Can't show alert:" + text);
-        }
-        Logger.log(text);
     }
     
 }
