312ccfe86d990f1ce3fb5b1ce9fa8ca5c10d77e8
vipoll <vipollmail@gmail.com>
2025-03-03 10:39:41 +0400

    desktop: add --debug command line argument

--- desktop/src/main/java/com/vipaol/MobappGameDesktopMain.java
+++ desktop/src/main/java/com/vipaol/MobappGameDesktopMain.java
@@ -1,5 +1,6 @@
 package com.vipaol;
 
+import mobileapplication3.game.DebugMenu;
 import mobileapplication3.game.MenuCanvas;
 import mobileapplication3.platform.Platform;
 import mobileapplication3.platform.ui.RootContainer;
@@ -11,6 +12,24 @@ import java.awt.event.WindowEvent;
 public class MobappGameDesktopMain extends Frame {
 
     public static void main(String[] args) {
+        if (args != null) {
+            for (int i = 0; i < args.length; i++) {
+                String arg = args[i];
+                if (arg != null) {
+                    if (arg.startsWith("--")) {
+                        arg = arg.substring(2);
+                    }
+                    if (arg.startsWith("/")) {
+                        arg = arg.substring(1);
+                    }
+                    switch (arg) {
+                        case "debug":
+                            DebugMenu.isDebugEnabled = true;
+                            break;
+                    }
+                }
+            }
+        }
         new MobappGameDesktopMain();
     }
 
