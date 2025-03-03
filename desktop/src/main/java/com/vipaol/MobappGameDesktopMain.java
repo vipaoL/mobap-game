8197b2882225d7e5ae43f30dbbc195b5a6b6a093
vipoll <vipollmail@gmail.com>
2025-03-03 14:58:14 +0400

    fixup! desktop: add --debug command line argument

--- desktop/src/main/java/com/vipaol/MobappGameDesktopMain.java
+++ desktop/src/main/java/com/vipaol/MobappGameDesktopMain.java
@@ -13,8 +13,7 @@ public class MobappGameDesktopMain extends Frame {
 
     public static void main(String[] args) {
         if (args != null) {
-            for (int i = 0; i < args.length; i++) {
-                String arg = args[i];
+            for (String arg : args) {
                 if (arg != null) {
                     if (arg.startsWith("--")) {
                         arg = arg.substring(2);
