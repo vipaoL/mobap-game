9c857d5a9a7ac6ee1d172fb5124aa2ec9117ef6a
vipoll <vipollmail@gmail.com>
2025-03-24 15:03:07 +0400

    migrate records to the new storage

--- android/app/src/main/java/com/vipaol/mobapp/android/GameActivity.java
+++ android/app/src/main/java/com/vipaol/mobapp/android/GameActivity.java
@@ -1,11 +1,38 @@
 package com.vipaol.mobapp.android;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
 import mobileapplication3.game.MenuCanvas;
+import mobileapplication3.platform.Platform;
 import mobileapplication3.platform.ui.MobappActivity;
 import mobileapplication3.ui.IUIComponent;
 import mobileapplication3.ui.UISettings;
 
 public class GameActivity extends MobappActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // ------- migrate records to use a unified way to storing them
+        try {
+            // try to read from the new storage
+            String records = Platform.readStoreAsString("records");
+
+            // if the new storage is empty...
+            if (records == null || records.equals("")) {
+                // read from the old storage
+                SharedPreferences oldPrefs = getSharedPreferences("Records", Context.MODE_PRIVATE);
+                records = oldPrefs.getString("records", "");
+                // save to the new place
+                Platform.storeString(records, "records");
+            }
+        } catch (Exception ex) {
+            Platform.showError("Can't migrate records!", ex);
+        }
+        // -------
+    }
 
     @Override
     protected IUIComponent getRootUIComponent() {
--- j2me/src/mobileapplication3/GameMIDlet.java
+++ j2me/src/mobileapplication3/GameMIDlet.java
@@ -4,10 +4,13 @@
  */
 package mobileapplication3;
 
+import javax.microedition.rms.RecordStore;
+import javax.microedition.rms.RecordStoreNotFoundException;
 import mobileapplication3.game.MenuCanvas;
 import mobileapplication3.platform.Logger;
 import mobileapplication3.platform.MobappMIDlet;
 import mobileapplication3.platform.Platform;
+import mobileapplication3.platform.Records;
 import mobileapplication3.platform.ui.RootContainer;
 
 /**
@@ -27,6 +30,58 @@ public class GameMIDlet extends MobappMIDlet {
         Logger.log("Main:constr");
         MenuCanvas menuCanvas = new MenuCanvas();
         RootContainer.setRootUIComponent(menuCanvas);
+
+        // ------- migrate records to use a unified way to storing them
+        try {
+            try {
+            	// try to read from the new storage
+            	int[] records = Records.getRecords();
+            	// if the new storage is empty...
+            	if (records == null || records.length == 0) {
+            		// old format and old storage
+                	tryMigrateRecordsFrom("Records");
+            	}
+            } catch (NumberFormatException ex) {
+            	// old format, but already moved to the new storage
+            	tryMigrateRecordsFrom("records");
+            }
+        } catch (Exception ex) {
+            Platform.showError("Can't migrate records!", ex);
+        }
+        // -------
+    }
+
+	private void tryMigrateRecordsFrom(String oldStoreName) {
+		try {
+			RecordStore oldStore = null;
+			int numRecords;
+			try {
+				oldStore = RecordStore.openRecordStore(oldStoreName, false);
+				numRecords = oldStore.getNumRecords();
+			} catch (RecordStoreNotFoundException ex) {
+				numRecords = 0;
+			}
+
+			if (numRecords > 0) {
+				int[] records = new int[numRecords];
+			    for (int i = 0; i < numRecords; i++) {
+				    records[i] = byteArrayToInt(oldStore.getRecord(i + 1));
+			    }
+			    oldStore.closeRecordStore();
+				RecordStore.deleteRecordStore(oldStoreName);
+
+			    for (int i = 0; i < numRecords; i++) {
+					 // save to the new place
+					Records.saveRecord(records[i], numRecords);
+			    }
+			}
+		} catch (Exception ex) {
+	        Platform.showError("Can't migrate records!", ex);
+	    }
+	}
+
+    public static int byteArrayToInt(byte[] bytes) {
+        return (bytes[0] << 24) | ((bytes[1] & 0xFF) << 16) | ((bytes[2] & 0xFF) << 8) | (bytes[3] & 0xFF);
     }
     
 }
