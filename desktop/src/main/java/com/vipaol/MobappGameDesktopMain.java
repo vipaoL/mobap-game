04c5649567b2145a859b9ce4293f596241ed452b
vipoll <vipollmail@gmail.com>
2025-04-20 14:05:52 +0400

    desktop: add command line options to override fullscreen mode and font size
    --font-size <Integer>          Override the default font size (default: 32)
    --fullscreen                   Enable fullscreen mode
    --no-fullscreen                Disable fullscreen mode

--- desktop/src/main/java/com/vipaol/MobappGameDesktopMain.java
+++ desktop/src/main/java/com/vipaol/MobappGameDesktopMain.java
@@ -7,6 +7,7 @@ import mobileapplication3.game.DebugMenu;
 import mobileapplication3.game.MenuCanvas;
 import mobileapplication3.platform.FileUtils;
 import mobileapplication3.platform.MobappDesktopMain;
+import mobileapplication3.platform.PlatformSettings;
 import mobileapplication3.platform.ui.RootContainer;
 
 import java.io.IOException;
@@ -20,11 +21,17 @@ public class MobappGameDesktopMain extends MobappDesktopMain {
 
     private static void parseArgs(String[] args) {
         OptionParser parser = new OptionParser();
-        OptionSpec<Void> debugOpt = parser.acceptsAll(Arrays.asList("debug", "d"), "Enable debug mode");
+        OptionSpec<Void> debug = parser.acceptsAll(Arrays.asList("debug", "d"), "Enable debug mode");
         OptionSpec<String> workdirOpt = parser.accepts("workdir", "Directory for game settings, records and custom content")
                 .withRequiredArg()
                 .describedAs("directory")
                 .defaultsTo(FileUtils.getStoragePath());
+        OptionSpec<Integer> fontSize = parser.accepts("font-size", "Override the default font size")
+                .withRequiredArg()
+                .ofType(Integer.class)
+                .defaultsTo(PlatformSettings.getFontSize());
+        OptionSpec<Void> fullscreenMode = parser.accepts("fullscreen", "Enable fullscreen mode");
+        OptionSpec<Void> disableFullscreenMode = parser.accepts("no-fullscreen", "Disable fullscreen mode");
         parser.acceptsAll(Arrays.asList("help", "h", "?"), "Show help").forHelp();
 
         try {
@@ -33,8 +40,16 @@ public class MobappGameDesktopMain extends MobappDesktopMain {
                 parser.printHelpOn(System.out);
                 System.exit(0);
             }
-            DebugMenu.isDebugEnabled = options.has(debugOpt);
+            DebugMenu.isDebugEnabled = options.has(debug);
             FileUtils.setStoragePath(options.valueOf(workdirOpt));
+            if (options.has(fullscreenMode)) {
+                PlatformSettings.setFullscreenModeOverride(true);
+            } else if (options.has(disableFullscreenMode)) {
+                PlatformSettings.setFullscreenModeOverride(false);
+            }
+            if (options.has(fontSize)) {
+                PlatformSettings.setFontSizeOverride(options.valueOf(fontSize));
+            }
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
             System.out.println();
