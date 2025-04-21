c1ca9e7fdad77a489ad106f3efaaf17456d6e0a2
vipoll <vipollmail@gmail.com>
2025-04-21 13:58:21 +0400

    Better looking about screen, code cleanup

--- src/mobileapplication3/game/AboutScreen.java
+++ src/mobileapplication3/game/AboutScreen.java
@@ -31,44 +31,42 @@ public class AboutScreen extends GenericMenu implements Runnable {
         URL2_PREVIEW,
         "Version: " + Platform.getAppVersion(),
         "Back"};
-    private int counter = 17;
-    private int scW, scH;
-    private int qrSide = 0;
-    private int margin = 0;
-    private Font font = Font.getFont(Font.FACE_SYSTEM, Font.STYLE_PLAIN, Font.SIZE_SMALL),
-            font3 = Font.getFont(Font.FACE_SYSTEM, Font.STYLE_PLAIN, Font.SIZE_LARGE);
-    private int fontH = font.getHeight();
-    private boolean bigQRIsDrawn = false;
-    
+
+    private final Font font = Font.getFont(Font.FACE_SYSTEM, Font.STYLE_PLAIN, Font.SIZE_SMALL);
     private Image qr, qrBig;
 
-    public AboutScreen() {
-    	loadParams(MENU_OPTS, 0, MENU_OPTS.length - 1, MENU_OPTS.length - 1);
-    }
-    
+    private final int fontH = font.getHeight();
+    private int w, h;
+    private int headerH, qrSide, menuH;
+    private boolean bigQRIsDrawn = false;
+    private int counter = 17;
+
     public void init() {
+        loadParams(MENU_OPTS, 0, MENU_OPTS.length - 1, MENU_OPTS.length - 1);
         setFirstDrawable(1);
+    }
+
+    public void postInit() {
         (new Thread(this, "about")).start();
     }
-    
+
     protected void onSetBounds(int x0, int y0, int w, int h) {
     	super.onSetBounds(x0, y0, w, h);
-        if (scW == w && scH == h && qr != null) {
+        if (this.w == w && this.h == h && qr != null) {
             return;
         }
 
-        scW = w;
-        scH = h;
-        qrSide = scH - fontH * (STRINGS.length + MENU_OPTS.length + 3);
+        this.w = w;
+        this.h = h;
+        qrSide = this.h - fontH * (STRINGS.length + MENU_OPTS.length) * 2;
         qrSide = Math.max(qrSide, 66);
         qrSide = Math.min(qrSide, w*7/8);
+        int totalTextH = this.h - qrSide;
 
-        margin = fontH/2;
+        menuH = totalTextH * MENU_OPTS.length / (MENU_OPTS.length + STRINGS.length);
+        headerH = totalTextH - menuH;
+        loadCanvasParams(0, h - menuH, this.w, menuH);
 
-        int headerAndQrH = drawHeaderAndQR(null);
-        int menuH = scH - headerAndQrH;
-        loadCanvasParams(0, h - menuH, scW, menuH);
-        
         try {
             qr = Image.createImage("/qr.png").scale(qrSide, qrSide);
         } catch (IOException ex) {
@@ -79,26 +77,17 @@ public class AboutScreen extends GenericMenu implements Runnable {
                 e.printStackTrace();
             }
         }
-        
+
         try {
-            qrBig = Image.createImage("/qr.png").scale(Math.min(scW, scH), Math.min(scW, scH));
+            qrBig = Image.createImage("/qr.png").scale(Math.min(this.w, this.h), Math.min(this.w, this.h));
         } catch (IOException ex) {
             try {
-                qrBig = Image.createImage("resource://qr.png").scale(Math.min(scW, scH), Math.min(scW, scH));
+                qrBig = Image.createImage("resource://qr.png").scale(Math.min(this.w, this.h), Math.min(this.w, this.h));
             } catch (IOException e) {
                 ex.printStackTrace();
                 e.printStackTrace();
             }
         }
-    }
-
-    public void destroyApp(boolean unconditional) {
-        isStopped = true;
-        Platform.exit();
-    }
-    
-    public void setIsPaused(boolean isPaused) {
-        this.isPaused = isPaused;
     }
 
     public void run() {
@@ -130,79 +119,81 @@ public class AboutScreen extends GenericMenu implements Runnable {
             }
         }
     }
-    
+
     protected void onPaint(Graphics g, int x0, int y0, int w, int h, boolean forceInactive) {
     	//   if qr isn't selected, repaint on each frame
         //
-        //   if big qr is open, draw it oncely,
-        // and then we don't need to refresh screen
+        //   if big qr is open, draw it once,
+        // and then we don't need to refresh screen anymore
         if (bigQRIsDrawn) {
             return;
         }
         g.setColor(0, 0, 0);
-        g.fillRect(0, 0, scW, scH);
-        int headerAndQrH = drawHeaderAndQR(g);
-        int menuH = scH - headerAndQrH;
+        g.fillRect(0, 0, this.w, this.h);
+        g.setFont(font);
+        drawHeader(g);
+        drawQR(g);
         super.onPaint(g, x0, y0 + h - menuH, w, menuH, forceInactive);
         tick();
-        
+
         if (selected == 0) {
             drawBigQR(g);
         } else {
             bigQRIsDrawn = false;
         }
     }
-    
-    int drawHeaderAndQR(Graphics g) {
-        if (g != null) {
-            g.setColor(255, 255, 255);
-        }
-        
-        int offset = margin;
+
+    private void drawHeader(Graphics g) {
+        g.setColor(0xffffff);
+        int dY = headerH / (STRINGS.length + 1);
         for (int i = 0; i < STRINGS.length; i++) {
-            if (g != null) {
-                g.setFont(font);
-                g.drawString(STRINGS[i], scW/2, offset, Graphics.HCENTER | Graphics.TOP);
-            }
-            offset += fontH;
+            int y = (i + 1) * dY;
+            g.drawString(STRINGS[i], w /2, y, Graphics.HCENTER | Graphics.VCENTER);
         }
-        offset += margin;
-        if (g != null && selected != 0) {
+    }
+
+    private void drawQR(Graphics g) {
+        if (selected != 0) {
             try {
-                g.drawImage(qr, scW / 2, offset, Graphics.HCENTER | Graphics.TOP);
+                g.drawImage(qr, w / 2, headerH, Graphics.HCENTER | Graphics.TOP);
             } catch (NullPointerException ex) {
-            	int leftX = scW / 2 - qrSide / 2;
-            	int rightX = scW / 2 + qrSide / 2;
-                g.drawLine(leftX, offset, rightX, offset);
-                g.drawLine(leftX, offset + qrSide - 1, rightX, offset + qrSide - 1);
-                g.drawLine(leftX, offset, leftX, offset + qrSide);
-                g.drawLine(rightX, offset, rightX, offset + qrSide);
-                int x = scW / 2;
-                int y = offset + qrSide / 2;
+                int leftX = w / 2 - qrSide / 2;
+                int topY = headerH;
+                int d = qrSide;
+                while (d > 0) {
+                    int c = 255 - 255 * d / qrSide;
+                    g.setColor(c, c, c);
+                    g.fillRect(leftX, topY, d, d);
+                    d -= 2;
+                    topY++;
+                    leftX++;
+                }
+
+                int x = w / 2;
+                int y = headerH + qrSide / 2;
+                g.setColor(0x000000);
                 g.drawString("Your ad", x, y, Graphics.HCENTER|Graphics.BOTTOM);
                 g.drawString("could be here.", x, y, Graphics.HCENTER|Graphics.TOP);
             }
         }
-        offset += qrSide;
-        return offset;
     }
-    
-    void drawBigQR(Graphics g) {
+
+    private void drawBigQR(Graphics g) {
         try {
-            g.drawImage(qrBig, scW / 2, scH / 2, Graphics.HCENTER | Graphics.VCENTER);
+            g.drawImage(qrBig, w / 2, h / 2, Graphics.HCENTER | Graphics.VCENTER);
         } catch (NullPointerException ex) {
             bigQRIsDrawn = true;
         }
     }
 
-    void openLink(String url) {
+    private void openLink(String url) {
         Logger.log(url);
         if (Platform.platformRequest(url)) {
             Platform.exit();
         }
     }
 
-    void selectPressed() {
+    protected void selectPressed() {
         int selected = this.selected;
         if (selected == MENU_OPTS.length - 4) {
             openLink(URL);
@@ -214,9 +205,6 @@ public class AboutScreen extends GenericMenu implements Runnable {
             counter+=1;
             if (counter >= 20) {
                 isStopped = true;
-                
-                WorldGen.isEnabled = true;
-                
                 World test3 = new World();
                 test3.setGravity(FXVector.newVector(10, 100));
                 GraphicsWorld.bgOverride = true;
